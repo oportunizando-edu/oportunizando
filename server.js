a249f34bcfe3c12bd7263bc9323bce7596762df0
Messias-Olivindo <messias.olivindo@sou.inteli.edu.br>
2025-06-30 18:54:02 -0300

    feat: configure server.js

--- server.js
+++ server.js
@@ -1,40 +1,21 @@
-require('dotenv').config();
+//Definição de constantes
 const express = require('express');
 const app = express();
 const path = require('path');
-const ServerErrorHandler = require('./helpers/serverErrorHandler');
 
+//Definição da views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from public directory
+//Definição do public
 app.use(express.static(path.join(__dirname, 'public')));
 
-async function startServer() {
-  const dbConnected = await ServerErrorHandler.databaseConnectionHandler();
-  
-  if (!dbConnected) {
-    process.exit(1);
-  }
+//Porta para ser usada
+const PORT = process.env.PORT || 3000;
 
-  app.use(express.json());
+//Avisar que está funcionando
+app.listen( PORT, () =>{
+  console.log("Servidor rodando na porta:", PORT);
+})
 
-  const userRoutes = require('./routes/userRoutes');
-  app.use('/users', userRoutes);
-
-  const frontendRoutes = require('./routes/frontRoutes');
-  app.use('/', frontendRoutes);
-
-  // Middleware para lidar com erros de rota não encontrada
-  app.use(ServerErrorHandler.notFoundHandler);
-
-  // Middleware para lidar com erros internos do servidor
-  app.use(ServerErrorHandler.globalErrorHandler);
-
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando na porta ${PORT}`);
-  });
-}
-
-startServer();
+module.exports = app;
