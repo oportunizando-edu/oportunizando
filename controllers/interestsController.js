cced0718eb730fa549eff764d6b08687f26daf55
Arielly <maria.lima@sou.inteli.edu.br>
2025-07-09 20:18:17 -0300

    fix: caminho para oportunities

--- controllers/interestsController.js
+++ controllers/interestsController.js
@@ -2,7 +2,7 @@ const interestsModel = require('../models/interestsModel');
 
 //selecionar areas de interesse
 exports.selectAreas = async (req, res) => {
-    const user_id = req.session.user?.user_id;//pega da sessão
+    const user_id = req.session.user?.user_id; //pega da sessão
     const { area_ids } = req.body;
 
   if (!user_id || !Array.isArray(area_ids) || area_ids.length === 0) {
@@ -11,7 +11,7 @@ exports.selectAreas = async (req, res) => {
 
   try {
     const result = await interestsModel.selectAreas(user_id, area_ids);
-    return res.status(201).json({ message: 'Áreas de interesse inseridas com sucesso', data: result });
+     res.redirect('/oportunities');
   } catch (err) {
     return res.status(500).json({ erro: err.message });
   }
